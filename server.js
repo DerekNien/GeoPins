68b07a98bff789aac32ec01b5b473bbfccf8aae8
DerekNien <aegis60918@gmail.com>
2019-03-27 22:06:51 +0800

    - Add: connection to mongo atlas db

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-import { ApolloServer } from 'apollo-server';
+import {
+  ApolloServer
+} from 'apollo-server';
 import typeDefs from './typeDefs';
 import resolvers from './resolvers';
+import mongoose from 'mongoose';
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('DB connected'))
+  .catch(err => console.error(err));
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
-server.listen().then(({url}) => {
+server.listen().then(({
+  url
+}) => {
   console.log(`server listening on ${url}`);
 });
