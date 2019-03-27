8184283d003330f5fb4a46178a38479cf4a03063
DerekNien <aegis60918@gmail.com>
2019-03-27 22:14:50 +0800

    - Add: mongoose User and Pin model

--- typeDefs.js
+++ typeDefs.js
@@ -8,6 +8,23 @@ export default gql`
     picture: String
   }
 
+  type Pin {
+    _id: ID
+    title: String
+    content: String
+    image: String
+    latitude: Float
+    longitude: Float
+    auther: User
+    comments: [Comment]
+  }
+
+  type Comment {
+    text: String
+    createdAt: String
+    auther: User
+  }
+
   type Query {
     me: User
   }
