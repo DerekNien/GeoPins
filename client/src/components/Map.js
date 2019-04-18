0f165f981cf14a44da2126e59b673f8635de5e69
DerekNien <aegis60918@gmail.com>
2019-04-18 21:24:38 +0800

    - Add: media query

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -5,6 +5,7 @@ import differenceInMinutes from "date-fns/difference_in_minutes";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+import { unstable_useMediaQuery as useMediaQuery } from "@material-ui/core/useMediaQuery";
 import { Subscription } from "react-apollo";
 
 import { useClient } from "../client";
@@ -27,6 +28,7 @@ const INITIAL_VIEWPORT = {
 
 const Map = ({ classes }) => {
   const client = useClient();
+  const mobileSize = useMediaQuery("(max-width: 650px)");
   const { state, dispatch } = useContext(Context);
 
   useEffect(() => {
@@ -89,12 +91,13 @@ const Map = ({ classes }) => {
   };
 
   return (
-    <div className={classes.root}>
+    <div className={mobileSize ? classes.rootMobile : classes.root}>
       <ReactMapGL
         width="100vw"
         height="calc(100vh - 64px)"
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxApiAccessToken="pk.eyJ1IjoiYWVnaXM2MDkxOCIsImEiOiJjanU1a24zanQxYTJtM3lvNzN6bW92dmlzIn0.fPIxrwgSFLMG2K9pyoZ7ng"
+        scrollZoom={!mobileSize}
         onViewStateChange={newViewport => setViewport(newViewport.viewState)}
         onClick={handleMapClick}
         {...viewport}
